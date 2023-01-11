eee74acedc81475639e4b2f2911450c0349178ca
kondi7 <wilkkonrad98@gmail.com>
2023-01-11 22:12:05 +0100

    Added sorting test

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, Locator
+from playwright.sync_api import Page, Locator, expect
 
 
 class InventoryPage:
@@ -6,18 +6,36 @@ class InventoryPage:
         self.page = page
 
     @property
-    def add_to_cart(self) -> Locator:
+    def add_to_cart_button(self) -> Locator:
         return self.page.locator('button:near(.inventory_item)')
 
     @property
     def number_of_all_items_in_cart(self) -> int:
-        return self.add_to_cart.count()
+        return self.add_to_cart_button.count()
 
     @property
     def cart(self) -> Locator:
         return self.page.locator(f"a:has-text(\"{self.number_of_all_items_in_cart - 1}\")")
 
+    @property
+    def sort_options(self) -> Locator:
+        return self.page.locator("[data-test=\"product_sort_container\"]")
+
+    @property
+    def items_available(self) -> Locator:
+        return self.page.locator('.inventory_item')
+
+    @property
+    def item_select(self):
+        return self.page.query_selector('.inventory_item')
+
     def add_all_items_to_cart(self) -> None:
         for i in range(self.number_of_all_items_in_cart):
-            self.add_to_cart.nth(i).click()
+            self.add_to_cart_button.nth(i).click()
         self.cart.click()
+
+    def name_z_to_a_option(self) -> None:
+        initial_position = self.item_select.bounding_box()
+        self.sort_options.select_option("za")
+        updated_position = self.item_select.bounding_box()
+        assert updated_position == initial_position
--- tests/test_sort_items_by_name_and_price.py
+++ tests/test_sort_items_by_name_and_price.py
@@ -1,10 +1,10 @@
 from playwright.sync_api import Playwright
 from pages.main_page import MainPage
-from pages.inventory_page import InventoryPage
 from settings.fixtures import user_data
+from pages.inventory_page import InventoryPage
 
 
-def test_adding_items_to_cart(user_data: dict, playwright: Playwright):
+def test_sorting(user_data: dict, playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
@@ -14,7 +14,7 @@ def test_adding_items_to_cart(user_data: dict, playwright: Playwright):
     page.pause()
     inventory_page = InventoryPage(page)
     main_page.login_to_store(user_data)
-    inventory_page.add_all_items_to_cart()
-    context.tracing.stop(path='test_results/trace_test_adding_items_to_cart.zip')
+    inventory_page.name_z_to_a_option()
+    context.tracing.stop(path='test_results/trace_test_sort_items.zip')
     context.close()
     browser.close()
